43b2880b836346c81b350e0454f115743cc7e263
Linh Pham <linh.pham@platform.inc>
2022-06-02 16:37:32 +0700

    done admin edit contest

--- html/pages/admin.edit.contest.php
+++ html/pages/admin.edit.contest.php
@@ -5,9 +5,16 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="../../css/admin.create.contest.css">
+
+	<link rel="stylesheet" href="../../css/admin.edit.css">
+
+	<link rel="stylesheet" href="../../css/admin.edit.challenge.css">
+
+	<link rel="stylesheet" href="../../css/admin.edit.contest.css">
 
 	<link rel="stylesheet" href="../../css/admin.create.challenge.css">
+
+	<link rel="stylesheet" href="../../css/admin.create.contest.css">
 
 	<link rel="stylesheet" href="../../css/style.css">
 
@@ -25,47 +32,66 @@
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
 
-	<title>Create Contest</title>
+	<title>Edit Challenge</title>
 </head>
 
 <body>
 	<div class="page-content-wrapper">
 		<?php require_once "../components/header.php" ?>
-		<div class="admin-create-contest-page-title-wrapper" style="display: block;">
-			<div class="admin-create-contest-page-title">
+		<div class="admin-edit-page-title-wrapper" style="display: block;">
+			<div class="admin-edit-page-title">
 				<div>
-					<div class="admin-create-contest-page-title-content">
+					<div class="admin-edit-page-title-content">
 						<span><a href="/">Manage Contests</a></span>
 						<i class="fa fa-angle-right" aria-hidden="true"></i>
-						<span class="current-page"><a>Create</a></span>
+						<span class="current-page"><a>Edit Contest Name</a></span>
 					</div>
 				</div>
 			</div>
 		</div>
-		<div class="admin-create-contest-page-content" style="min-height: 800px;">
+		<div class="admin-edit-page-content" style="min-height: 800px;">
 			<div>
-				<section class="admin-create-contest-container">
-					<div class="admin-create-challenge-page-header" style="width: 100%;">
-						<h1>Create Contest</h1>
-					</div>
-					<div style="padding-bottom: 5px; background-color: #ffffff;"></div>
+				<section class="admin-edit-container">
+					<header>
+						<div class="admin-edit-challenge-name">
+							<h1>
+								<div>Contest Name</div>
+							</h1>
+						</div>
+						<div class="admin-edit-tabs">
+							<ul>
+								<li class="admin-edit-tabs-item-active">
+									<a href="/">Details</a>
+								</li>
+								<li>
+									<a href="/">Moderator</a>
+								</li>
+								<li>
+									<a>Add/Remove Challenges</a>
+								</li>
+								<li>
+									<a href="/">ABC</a>
+								</li>
+							</ul>
+						</div>
+					</header>
 					<div class="admin-create-challenge-page-inner-body" style="position: relative;">
-						<p>Get started by providing the initial details needed to create a contests.</p>
+						<p>This is the basic information that describes your contest.</p>
 						<div class="admin-create-challenge-page-inner-body-form-element">
-							<label for="admin-create-contest-name" class="admin-create-challenge-page-form-element-challenge-label">Contest Name</label>
+							<label for="admin-edit-contest-name" class="admin-create-challenge-page-form-element-challenge-label">Contest Name</label>
 							<div class="admin-create-challenge-page-form-element-challenge-name-input">
-								<input id="admin-create-contest-name" type="text">
+								<input id="admin-edit-contest-name" type="text">
 							</div>
 						</div>
 						<div class="admin-create-challenge-page-inner-body-form-element">
-							<label for="admin-create-contest-description" class="admin-create-challenge-page-form-element-challenge-label">Description</label>
+							<label for="admin-edit-contest-description" class="admin-create-challenge-page-form-element-challenge-label">Description</label>
 							<div class="admin-create-challenge-page-form-element-challenge-description-input">
-								<textarea rows="2" id="admin-create-contest-description" class="admin-create-challenge-description-input" placeholder="Write a short summary about the contest" spellcheck="false"></textarea>
+								<textarea rows="2" id="admin-edit-contest-description" class="admin-create-challenge-description-input" placeholder="Write a short summary about the contest" spellcheck="false"></textarea>
 								<small class="description-input-help-hint">Characters left: 140</small>
 							</div>
 						</div>
 						<div class="admin-create-challenge-page-inner-body-form-element">
-							<label for="admin-create-contest-statement" class="admin-create-challenge-page-form-element-challenge-label">Contest Detail Statement</label>
+							<label for="admin-edit-contest-statement" class="admin-create-challenge-page-form-element-challenge-label">Contest Detail Statement</label>
 							<div class="admin-create-challenge-page-form-element-challenge-description-input">
 								<div>framework editor</div>
 								<textarea rows="8" spellcheck="false"></textarea>
@@ -73,36 +99,37 @@
 							</div>
 						</div>
 						<div class="admin-create-challenge-page-inner-body-form-element">
-							<label for="admin-create-contest-start-time" class="admin-create-challenge-page-form-element-challenge-label">Start Time</label>
+							<label for="admin-edit-contest-start-time" class="admin-create-challenge-page-form-element-challenge-label">Start Time</label>
 							<div class="admin-create-challenge-page-form-element-challenge-name-input">
-								<input id="admin-create-contest-start-time-date" type="date">&nbsp; at &nbsp; <input id="admin-create-contest-start-time-hour" type="time">
+								<input id="admin-edit-contest-start-time-date" type="date">&nbsp; at &nbsp; <input id="admin-edit-contest-start-time-hour" type="time">
 							</div>
 						</div>
 						<div class="admin-create-challenge-page-inner-body-form-element">
-							<label for="admin-create-contest-end-time" class="admin-create-challenge-page-form-element-challenge-label">End Time</label>
+							<label for="admin-edit-contest-end-time" class="admin-create-challenge-page-form-element-challenge-label">End Time</label>
 							<div class="admin-create-challenge-page-form-element-challenge-name-input">
-								<input id="admin-create-contest-end-time-date" type="date">&nbsp; at &nbsp; <input id="admin-create-contest-end-time-hour" type="time">
+								<input id="admin-edit-contest-end-time-date" type="date">&nbsp; at &nbsp; <input id="admin-edit-contest-end-time-hour" type="time">
 							</div>
 						</div>
 						<div class="admin-create-challenge-page-inner-body-form-element">
-							<label for="admin-create-contest-duration" class="admin-create-challenge-page-form-element-challenge-label">Duration</label>
+							<label for="admin-edit-contest-duration" class="admin-create-challenge-page-form-element-challenge-label">Duration</label>
 							<div class="admin-create-challenge-page-form-element-challenge-name-input">
-								<input id="admin-create-contest-duration" type="number" min=1>&nbsp; minutes
+								<input id="admin-edit-contest-duration" type="number" min=1>&nbsp; minutes
 							</div>
 						</div>
 					</div>
-				</section>
+					</section>
 				<div class="admin-create-challenge-sticky-bottom">
 					<div class="admin-create-challenge-sticky-bottom-bar">
 						<div class="admin-create-challenge-sticky-bottom-button">
-							<button class="admin-create-challenge-save-button" id="admin-create-contest-save-button">Save Changes</button>
+							<button class="admin-create-challenge-save-button" id="admin-edit-contest-save-button">Save Changes</button>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-	<script src="../../js/admin.create.contest.js"></script>
+	</div>
+	<script src="../../js/admin.edit.contest.js"></script>
 </body>
 
 </html>
